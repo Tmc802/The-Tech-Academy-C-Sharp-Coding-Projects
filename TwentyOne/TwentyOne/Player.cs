dc9ac7401d2d29de7f01058dcf4bcc2c790dd550
Tyler Mc <tmcintyre802@gmail.com>
2018-11-17 10:01:23 -0700

    Twenty One Draft
    a draft version of my twenty-one game in c#

---
+++ TwentyOne/TwentyOne/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Player
+    {
+        public Player(string name, int begginingBalance)
+        {
+            Hand = new List<Card>();
+            Balance = begginingBalance;
+            Name = name;
+        }
+
+        public List<Card> Hand { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public bool isActivelyPlaying { get; set; }
+
+        public static Game operator+ (Game game, Player player)
+        {
+            game.Players.Add(player);
+            return game;
+        }
+        public static Game operator- (Game game, Player player)
+        {
+            game.Players.Remove(player);
+            return game;
+        }
+    }
+}
---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // greet the user and prompt for their name
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+
+            // inquire how much money the user will be playing with
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            // prompt the user if they'd like to start a new game
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "Yes" || answer == "yeah" || answer == "y" || answer == "ya") {
+
+                // istantiate the players information with the user entries above
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+
+                // while loop to know if the player is playing the game
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            //end game and exit while loop prompt
+            Console.WriteLine("Feel free to look around the casino, Bye for now.");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
