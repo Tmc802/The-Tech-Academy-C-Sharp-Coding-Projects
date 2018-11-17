bd8c7f84b29c284ed8208625d92cbbf72559f4c3
Tyler Mc <tmcintyre802@gmail.com>
2018-11-17 11:12:58 -0700

    TwentyOne update
    An update to my TwentyOne game where i've added features to check both the players hand for blackjack and the dealers hand

--- TwentyOne/TwentyOne/TwentyOneGame.cs
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -12,17 +12,22 @@ namespace TwentyOne
 
         public override void Play()
         {
+            // instantiates new dealer in game
             Dealer = new TwentyOneDealer();
             foreach (Player player in Players)
             {
                 player.Hand = new List<Card>();
                 player.Stay = false;
             }
+            // dealer properties
             Dealer.Hand = new List<Card>();
             Dealer.Stay = false;
             Dealer.Deck = new Deck();
+
+            // asks each player to place a bet
             Console.WriteLine("Place your bet");
 
+            // foreach loop adds each players bet to the pool
             foreach (Player player in Players)
             {
                 int bet = Convert.ToInt32(Console.ReadLine());
@@ -40,10 +45,37 @@ namespace TwentyOne
                 {
                     Console.WriteLine("{0}: ", player.Name);
                     Dealer.Deal(player.Hand);
+
                     // logic check for blackjack
                     if (i == 1)
                     {
+                        bool blackJack = TwentyOneRules.CheckForBlackJack(player.Hand);
+                        if (blackJack)
+                        {
+                            // sends message player has 21
+                            Console.WriteLine("Blackjack! {0} wins {1}", player.Name, Bets[player]);
 
+                            // returns the players bet plus 1.5 times the winnings from other players bets
+                            player.Balance += Convert.ToInt32((Bets[player] * 1.5 + Bets[player]));
+                            //ends round
+                            return;
+                        }
+                    }
+                }
+                Console.Write("Dealer: ");
+
+                //checks dealers hand for blackjack
+                Dealer.Deal(Dealer.Hand);
+                if (i == 1)
+                {
+                    bool blackJack = TwentyOneRules.CheckForBlackJack(Dealer.Hand);
+                    if (blackJack)
+                    {
+                        Console.WriteLine("Dealer has BlackJack! Everyone Loses!");
+                        foreach (KeyValuePair<Player, int> entry in Bets)
+                        {
+                            Dealer.Balance += entry.Value;
+                        }
                     }
                 }
             }
--- TwentyOne/TwentyOne/TwentyOneRules.cs
+++ TwentyOne/TwentyOne/TwentyOneRules.cs
@@ -34,12 +34,21 @@ namespace TwentyOne
             int value = Hand.Sum(x => _cardValues[x.Face]);
             result[0] = value;
             if (result.Length == 1) return result;
+            for (int i = 1; i < result.Length; i++)
+            {
+                value += (i * 10);
+                result[i] = value;
+            }
+            return result;
         }
 
         // check for black jack using a boolean value and list
         public static bool CheckForBlackJack(List<Card> Hand)
         {
-
+            int[] possibleValues = GetAllPossibleHandValues(Hand);
+            int value = possibleValues.Max();
+            if (value == 21) return true;
+            else return false;
         }
     }
 }
