2e9c8b6df6b1b9c7b1cf9dc9545160f30c6b60ac
Tyler Mc <tmcintyre802@gmail.com>
2018-11-17 10:57:05 -0700

    TwentyOne update
    an update of my Twenty One game where i've added some logic checks and started to sort all possible card values

--- TwentyOne/TwentyOne/TwentyOneRules.cs
+++ TwentyOne/TwentyOne/TwentyOneRules.cs
@@ -8,6 +8,38 @@ namespace TwentyOne
 {
     public class TwentyOneRules
     {
+        // use dictionary to set card values.
+        private static Dictionary<Face, int> _cardValues = new Dictionary<Face, int>()
+        {
+            [Face.Two] = 2,
+            [Face.Three] = 3,
+            [Face.Four] = 4,
+            [Face.Five] = 5,
+            [Face.Six] = 6,
+            [Face.Seven] = 7,
+            [Face.Eight] = 8,
+            [Face.Nine] = 9,
+            [Face.Ten] = 10,
+            [Face.Jack] = 10,
+            [Face.Queen] = 10,
+            [Face.King] = 10,
+            [Face.Ace] = 1
+        };
+        //list all possible values with an array
+        public static int[] GetAllPossibleHandValues (List<Card> Hand)
+        {
+            // use lambda expression to get hand values
+            int aceCount = Hand.Count(x => x.Face == Face.Ace);
+            int[] result = new int[aceCount + 1];
+            int value = Hand.Sum(x => _cardValues[x.Face]);
+            result[0] = value;
+            if (result.Length == 1) return result;
+        }
 
+        // check for black jack using a boolean value and list
+        public static bool CheckForBlackJack(List<Card> Hand)
+        {
+
+        }
     }
 }
