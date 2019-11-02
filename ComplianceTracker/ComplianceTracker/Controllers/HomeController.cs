b60a30d48980dbdc0a253d8b63edc6f3bdae8b68
Tyler Mc <tmcintyre802@gmail.com>
2019-11-01 19:54:03 -0600

    Compliance Tracker MVC Web API
    Initial upload for Compliance Tracker. This upload is for the UI that will allow the user to login and start to create his/her customized checklist for compliance standards. Also features info about the company.

---
+++ ComplianceTracker/ComplianceTracker/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComplianceTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View("About");
+        }
+
+        public ActionResult HowItWorks()
+        {
+            return View("HowItWorks");
+        }
+
+        public ActionResult Pricing()
+        {
+            return View("Pricing");
+        }
+    }
+}
---
+++ ComplianceTracker/ComplianceTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ComplianceTracker.MvcApplication" Language="C#" %>
