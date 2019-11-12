596da9288d697089519eafde358e1a4fe3014111
Tyler Mc <tmcintyre802@gmail.com>
2019-11-11 17:50:32 -0700

    Data File Parser initial upload
    A data file parser web application. Work in progress.

---
+++ FileDataExtractor/FileDataExtractor/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using FileDataExtractor.Models;
+using LumenWorks.Framework.IO;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
+using System.Data;
+using System;
+
+namespace FileDataExtractor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChooseFile()
+        {
+            return View();
+        }
+
+        public ActionResult ViewData(string url)
+        {
+            //var url = "https://data.cityofnewyork.us/api/views/kku6-nxdu/rows.csv?accessType=DOWNLOAD";
+            //Method 1
+            GetCSVFile newURL = new GetCSVFile();
+
+            DataTable dt = newURL.GetCSV(url); // feed the url into the parser
+
+            var Model = dt; // pass the data to a datatable object "Model"
+
+            return View(Model);
+        }
+
+        public ActionResult FilteredDataView(string column, string sign, int operand, string url, string sortOrder)
+        {
+
+            // parse the url again
+            GetCSVFile newURL = new GetCSVFile();
+             
+            DataTable dt = newURL.GetCSV(url); // feed the url into the parser
+
+            DataTable filtDataTable = new DataTable(); // create a datatable to display the results of the filtered data
+
+            // send the column names to a new filtered data table
+            foreach(DataColumn col in dt.Columns)
+            {
+                filtDataTable.Columns.Add(col.ColumnName, col.DataType);
+            }
+
+            // Create the expression
+            string expression = "[" + column + "]" + sign + operand;
+            DataRow[] foundRows; // add the filtered data to a datarow array
+            
+            // Use the Select method to find all rows matching the filter.
+            foundRows = dt.Select(expression);
+
+            //Add the array foundRows to a new datatable
+            foreach (DataRow row in foundRows)
+            {
+                var r = filtDataTable.Rows.Add();
+                foreach (DataColumn col in filtDataTable.Columns)
+                {
+                    r[col.ColumnName] = row[col.ColumnName];
+                }
+            }
+
+            // set the sort order of filtered data
+            filtDataTable.DefaultView.Sort = "[" +column+ "]"+ " " + sortOrder;
+            // set the sorted data to a new DataTable object
+            DataTable dtSorted = filtDataTable.DefaultView.ToTable();
+
+            var Model = dtSorted;
+
+            return View(Model);
+            }
+        }
+}
---
+++ FileDataExtractor/FileDataExtractor/Models/GetCSVFile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using LumenWorks.Framework.IO.Csv;
+
+namespace FileDataExtractor.Models
+{
+
+
+
+    public class GetCSVFile
+    {
+        //public List<string> parsedCSV = new List<string>();        
+        
+        public DataTable GetCSV(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url); // web request
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse(); // get response
+            StreamReader sr = new StreamReader(resp.GetResponseStream()); // response stream? 
+            DataTable dtCsv = new DataTable();
+            DataTable dt = new DataTable();
+
+            while (!sr.EndOfStream)
+            {
+                string fullText = sr.ReadToEnd(); // read full file text
+                string[] rows = fullText.Split('\n'); // split full file text into
+
+                for (int i = 0; i < rows.Count() - 1; i++)
+                {
+                    string[] rowValues = rows[i].Split(',');//split each row with,
+                    {
+                        if (i == 0)
+                        {
+                            for (int j = 0; j < rowValues.Count(); j++)
+                            {
+                                dtCsv.Columns.Add(rowValues[j], typeof(double)); // add headers
+                            }
+                        }
+                        else
+                        {
+                            DataRow dr = dtCsv.NewRow();
+                            for (int k = 0; k < rowValues.Count(); k++)
+                            {
+                                var rowV = rowValues[k].ToString();
+
+                                dr[k] = Convert.ToDouble(rowV);
+                            }
+                            dtCsv.Rows.Add(dr); //add other rows
+                        }
+                    }
+                }
+            }
+                sr.Close();
+
+            return dtCsv;
+        }
+    }
+}
